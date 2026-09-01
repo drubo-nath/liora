72ef8d5ab2d778d3a8ce5ef66c8db52e6e8796e3
drubo-nath <drubo408@gmail.com>
2026-09-01 20:30:50 +0800

    feat: integrate Meta Pixel (1075296681620135) with noscript fallback
    - Hardcode pixel ID 1075296681620135 as default fallback
    - Add official noscript img tag for JS-disabled browsers
    - AddToCart and Purchase conversion events already wired up

--- src/components/analytics/Analytics.tsx
+++ src/components/analytics/Analytics.tsx
@@ -2,9 +2,11 @@
 
 import Script from "next/script";
 
+const META_PIXEL_ID =
+  process.env.NEXT_PUBLIC_META_PIXEL_ID || "1075296681620135";
+
 export default function Analytics() {
   const gaId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
-  const pixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID;
 
   return (
     <>
@@ -28,24 +30,33 @@ export default function Analytics() {
         </>
       )}
 
-      {/* Meta (Facebook/Instagram) Pixel */}
-      {pixelId && (
-        <Script id="meta-pixel-init" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '${pixelId}');
-            fbq('track', 'PageView');
-          `}
-        </Script>
-      )}
+      {/* Meta (Facebook/Instagram) Pixel — official base code */}
+      <Script id="meta-pixel-init" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '${META_PIXEL_ID}');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+
+      {/* Meta Pixel noscript fallback (for browsers with JS disabled) */}
+      <noscript>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+          alt=""
+        />
+      </noscript>
     </>
   );
 }
-
